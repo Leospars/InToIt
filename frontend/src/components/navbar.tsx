6a9785a46625b5dd1b60e6324b47b523b0e0407c
AyoXavi <33073005+AyoXavi@users.noreply.github.com>
2026-03-14 19:52:35 -0400

    Google OAuth, useSession hook

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,6 +1,12 @@
+import { useSession } from "@/hooks/useSession";
+import { supabase } from "@/lib/supabase";
 
 const Navbar = () => {
+  const session = useSession();
 
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
 
   return (
     <header className="flex h-16 w-full items-center justify-between border-b border-gray-200 px-4">
@@ -9,16 +15,39 @@ const Navbar = () => {
         IntoIt
       </div>
 
+      {
+        !!session
+          ? <div className="flex items-center gap-2">
+
+            <div>
+              Hello {
+                session.user.user_metadata.username
+                ??
+                session.user.user_metadata.full_name
+                ??
+                session.user.email
+              }!
+            </div>
+
+            <button
+              className="inline-flex items-center justify-center bg-gray-950 text-gray-50 h-8 rounded-lg px-3 text-sm"
+              onClick={signOut}
+            >
+              Sign out
+            </button>
+
+          </div>
+          :
           <div className="flex items-center gap-2">
 
             <button className="inline-flex items-center justify-center bg-white text-gray-950 shadow hover:bg-gray-100 h-8 rounded-lg px-3 text-sm">
               Feedback
             </button>
 
-           
+
 
           </div>
-
+      }
 
     </header>
   );
---
+++ frontend/src/hooks/useSession.tsx
@@ -0,0 +1,27 @@
+import { Session } from "@supabase/supabase-js";
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+
+export function useSession() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+
+    async function fetchInitialSession() {
+      const { data } = await supabase.auth.getSession();
+
+      if (data) {
+        setSession(data.session);
+      }
+    }
+
+    fetchInitialSession();
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return session;
+}
