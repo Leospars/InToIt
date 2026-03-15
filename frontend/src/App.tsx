6e43d4c14f70de317dfd1118cd4bde4cb31cfcda
cajaun <cajaun.campbell@gmail.com>
2026-03-15 13:04:38 -0500

    Fixing app analytics user error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,11 @@ import Quiz from "./pages/quiz";
 import Shorts from "./pages/shorts";
 import TopicPage from "./pages/topics";
 import Upload from "./pages/upload";
+import { useAuth } from "./context/auth-context";
 
 const App = () => {
+
+  const {user} = useAuth()
   return (
     <div className="flex flex-col h-screen overflow-hidden bg-[color(display-p3_0.975_0.975_0.975)]">
 
@@ -30,7 +33,7 @@ const App = () => {
     
             <Route path="/shorts" element={<Shorts />} />
             <Route path="/course-outline" element={<CourseOutline />} />
-            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/analytics" element={user ? <Analytics userId={user.id} /> : null} />
             <Route path="/live-chat" element={<RootChat />} />
 <Route path="/upload" element={<Upload />} />
             <Route
